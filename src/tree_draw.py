2ed48d5c8f1b007387021e8e6506788e19ea1bdb
ChamiLamelas <chami.lamelas@gmail.com>
2022-11-13 16:08:56 -0500

    finished tree draw

--- src/tree_draw.py
+++ src/tree_draw.py
@@ -1,6 +1,7 @@
 import torchvision.datasets as datasets
 from PIL import Image, ImageDraw
 import numpy as np
+import random
 
 # Build sample tree
 mnist_trainset = datasets.MNIST(
@@ -12,7 +13,7 @@ def make_blank_im_arr():
 
 
 def get_mnist_image(digit):
-    return mnist_trainset[np.random.choice(np.where(mnist_trainset.targets == digit))]
+    return mnist_trainset[random.choice(np.where(mnist_trainset.targets == digit)[0])][0]
 
 
 def put_mnist(im_arr, mnist_im, top_left):
@@ -22,22 +23,33 @@ def put_mnist(im_arr, mnist_im, top_left):
            top_left[1]:top_left[1] + 28] = mnist_arr
 
 
-def circle_mnist(im, top_left):
+def rc_to_xy(rcs):
+    return [(c, r) for r, c in rcs]
+
+
+def circle_mnist(top_left, drawer):
     bbox = [(top_left[0] + 14 - (14 * (2 ** 0.5)), top_left[1] + 14 - (14 * (2 ** 0.5))),
             (top_left[0] + 14 + (14 * (2 ** 0.5)), top_left[1] + 14 + (14 * (2 ** 0.5)))]
-    print(top_left, bbox)
-    draw = ImageDraw.Draw(im)
-    draw.ellipse(bbox, fill=None)
+    drawer.ellipse(rc_to_xy(bbox), fill=None)
 
 
-# change me
-nodes = [(113, 113)]
+def draw_edge(drawer, start, end):
+    drawer.line(rc_to_xy([start, end]), fill=0)    
+
+
+nodes = [(50, 93), (100, 43), (100, 143), (150, 98), (150, 188)]
+digits = [2, 1, 4, 3, 5]
 
 im_arr = make_blank_im_arr()
-mnist_im = mnist_trainset[0][0]
-for n in nodes:
-    put_mnist(im_arr, mnist_im, (113, 113))
+mnist_ims = [get_mnist_image(d) for d in digits]
+for n, mnist_im in zip(nodes, mnist_ims):
+    put_mnist(im_arr, mnist_im, n)
 im = Image.fromarray(im_arr)
+draw = ImageDraw.Draw(im)
 for n in nodes:
-    circle_mnist(im, (113, 113))
-im.save('../data/test_drawings/sample_gentree.png')
+    circle_mnist(n, draw)
+draw_edge(draw, (78, 93), (100, 71))
+draw_edge(draw, (78, 121), (100, 143))
+draw_edge(draw, (128, 143), (150, 126))
+draw_edge(draw, (128, 171), (150, 188))
+im.save('../overleaf/images/sample_gentree.png')
