ac659476f0dc73118470bd657691452970afd223
ChamiLamelas <chami.lamelas@gmail.com>
2022-11-14 14:57:03 -0500

    add yolos paper, new picture

--- src/bt_tree_representation.py
+++ src/bt_tree_representation.py
@@ -6,6 +6,8 @@ class Node:
 
 # this can be used to build a vector representation of a tree that
 # can be used to rebuild a unique tree
+
+
 def tree_to_vector(root):
     queue = list()
     queue.append(root)
@@ -23,7 +25,7 @@ def tree_to_vector(root):
             else:
                 queue.append(None)
                 queue.append(None)
-            vector.append(tmp.data if tmp is not None else 0)
+            vector.append(tmp.data if tmp is not None else 10)
     return vector
 
 
@@ -31,18 +33,18 @@ def test_tree_to_vector():
     root = Node(1)
     root.left = Node(2)
     root.right = Node(3)
-    assert tree_to_vector(root) == [1, 2, 3, 0, 0, 0, 0]
+    assert tree_to_vector(root) == [1, 2, 3, 10, 10, 10, 10]
     root = Node(1)
     root.left = Node(2)
     root.left.right = Node(3)
-    assert tree_to_vector(root) == [1, 2, 0, 0,
-                                    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert tree_to_vector(root) == [1, 2, 10, 10,
+                                    3, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     root = Node(1)
     root.right = Node(2)
     root.right.left = Node(3)
     root.right.right = Node(4)
-    assert tree_to_vector(root) == [1, 0, 2, 0,
-                                    0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0]
+    assert tree_to_vector(root) == [1, 10, 2, 10,
+                                    10, 3, 4, 10, 10, 10, 10, 10, 10, 10, 10]
 
 
 def vector_to_tree(vector):
@@ -54,10 +56,10 @@ def vector_to_tree(vector):
         left_idx = 2 * idx + 1
         right_idx = 2 * idx + 2
         if left_idx < len(vector):
-            if vector[left_idx] != 0:
+            if vector[left_idx] != 10:
                 node.left = Node(vector[left_idx])
                 queue.append((left_idx, node.left))
-            if vector[right_idx] != 0:
+            if vector[right_idx] != 10:
                 node.right = Node(vector[right_idx])
                 queue.append((right_idx, node.right))
     return root
@@ -75,18 +77,18 @@ def test_vector_to_tree():
     root = Node(1)
     root.left = Node(2)
     root.right = Node(3)
-    assert tree_equals(vector_to_tree([1, 2, 3, 0, 0, 0, 0]), root)
+    assert tree_equals(vector_to_tree([1, 2, 3, 10, 10, 10, 10]), root)
     root = Node(1)
     root.left = Node(2)
     root.left.right = Node(3)
-    assert tree_equals(vector_to_tree([1, 2, 0, 0,
-                                       3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), root)
+    assert tree_equals(vector_to_tree([1, 2, 10, 10,
+                                       3, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]), root)
     root = Node(1)
     root.right = Node(2)
     root.right.left = Node(3)
     root.right.right = Node(4)
-    assert tree_equals(vector_to_tree([1, 0, 2, 0,
-                                       0, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0]), root)
+    assert tree_equals(vector_to_tree([1, 10, 2, 10,
+                                       10, 3, 4, 10, 10, 10, 10, 10, 10, 10, 10]), root)
 
 
 def main():
