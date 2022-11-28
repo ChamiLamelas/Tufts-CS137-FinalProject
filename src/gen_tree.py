331075b3e50c54f37850a806721dd9039c05a605
ChamiLamelas <chami.lamelas@gmail.com>
2022-11-27 21:33:21 -0500

    changed nb to go to b on w, added mnist

--- src/gen_tree.py
+++ src/gen_tree.py
@@ -1,22 +1,25 @@
 import graphviz
 import random
-import os 
+import os
 
 indices = [1, 3, 5, 7, 2, 0, 13, 15, 17, 4]
 
+
 def gen_tree(num_nodes, img_name, output_directory="tree_images"):
-    dot = graphviz.Digraph(img_name, graph_attr = {'imagepath': '../data/sample_mnist'})
+    dot = graphviz.Digraph(img_name, graph_attr={'imagepath': os.path.join(
+        os.getcwd(), '..', 'data', 'sample_mnist')})
 
     for n in range(0, num_nodes):
-        dot.node(str(n), str(n), image=f'image{n}.png')
+        dot.node(str(n), "", image=f'image{n}.png')
 
     for n in range(0, num_nodes):
-        if 2 * n + 1 <= num_nodes:
+        if 2 * n + 1 < num_nodes:
             dot.edge(str(n), str(2 * n + 1))
-        if 2 * n + 2 <= num_nodes:
+        if 2 * n + 2 < num_nodes:
             dot.edge(str(n), str(2 * n + 2))
 
     dot.render(directory=output_directory, format='png')
+
 
 def main():
     for i in range(10):
@@ -25,4 +28,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
