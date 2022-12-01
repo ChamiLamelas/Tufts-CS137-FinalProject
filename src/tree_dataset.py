875d3ef88dfb13c16c4b08861f15297ac2f9b2ee
ChamiLamelas <chami.lamelas@gmail.com>
2022-12-01 01:20:29 -0500

    updated TreeDataset to return proper grayscale images

--- src/tree_dataset.py
+++ src/tree_dataset.py
@@ -3,12 +3,18 @@ import gen_dataset as gd
 import os
 from PIL import Image
 import numpy as np
+import torchvision.transforms as transforms
+import torch
 
 
 class TreeDataset(Dataset):
     def __init__(self, directory):
         self.directory = directory
         self.num_elements = int(len(os.listdir(directory))/4)
+        self.transforms = transforms.Compose([
+            transforms.Grayscale(),
+            transforms.ToTensor()
+        ])
 
     def __len__(self):
         return self.num_elements
@@ -16,8 +22,13 @@ class TreeDataset(Dataset):
     def __getitem__(self, idx):
         image = Image.open(os.path.join(
             self.directory, f'{gd.DATANAME_PREFIX}{idx}{gd.IMAGE_SUFFIX}'))
+        image = image.convert('RGB')
         tree_label = np.fromfile(os.path.join(
             self.directory, f'{gd.DATANAME_PREFIX}{idx}{gd.TREE_LABEL_SUFFIX}'))
         digit_labels = np.fromfile(os.path.join(
             self.directory, f'{gd.DATANAME_PREFIX}{idx}{gd.DIGIT_LABELS_SUFFIX}'))
-        return {'image': image, 'tree_label': tree_label, 'digit_labels': digit_labels}
+        return {
+            'image': torch.squeeze(self.transforms(image)), 
+            'tree_label': tree_label, 
+            'digit_labels': digit_labels
+        }
